97eb99604079e16f2c5f2da53f8723f867d52def
norbertSviatko <informatikasviatko@gmail.com>
2022-06-17 15:00:34 +0200

    'updated'

---
+++ client/src/components/partials/Navbar/NavItem.vue
@@ -0,0 +1,17 @@
+
+<template>
+
+    <li class="nav-item">{{ route }}</li>
+
+</template>
+
+<script>
+
+    export default {
+
+        name: 'NavItem',
+        props: ['route']
+
+    }
+
+</script>
---
+++ client/src/components/partials/Navbar/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+    import NavItem from "./NavItem.vue";
+
+</script>
+
+<template>
+
+    <nav id="nav-bar">
+
+        <NavItem route="Home" />
+        <NavItem route="About" />
+        <NavItem route="Contact" />
+
+    </nav>
+
+</template>
+
+<style>
+
+    #nav-bar {
+
+        max-width: 600px;
+        min-height: 2em;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        border: 2px solid red;
+        position: absolute;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+
+    }
+
+    .nav-item {
+
+        font-size: 24px;
+        color: black;
+        list-style: none;
+
+    }
+
+</style>
